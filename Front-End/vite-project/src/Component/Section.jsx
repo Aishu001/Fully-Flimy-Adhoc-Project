fd3d9fbd50bc66dece2e1d1f17b760afb59fbbc3
Aishu <sowandaryakrishnan@gmail.com>
2024-10-14 12:30:54 +0530

    updated home page

--- Front-End/vite-project/src/Component/Section.jsx
+++ Front-End/vite-project/src/Component/Section.jsx
@@ -1,41 +1,60 @@
-import React from 'react'
-import { styled } from '@mui/material/styles';
+import React from 'react';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid2';
-import '../Style/Section.css'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import '../Style/Section.css';
 
 function Section() {
-    const Item = styled(Paper)(() => ({
-     
-      }));
+  const imgCarosuel = ['three.jpg', 'one.jpg', 'two.png','four.jpg','five.png'];
 
-      const imgCarosuel = ['three.jpg','3 1c60a4cf-676c-4670-9f55-c72e5564ff97_600x600.jpg']
+  const settings = {
+    dots: true,             // Show dots for navigation
+    infinite: true,         // Loop infinitely
+    speed: 900,             // Increase speed for smoother animation
+    slidesToShow: 3,        // Show 3 slides at once
+    slidesToScroll: 1,      // Scroll one slide at a time
+    autoplay: true,         // Enable automatic sliding
+    autoplaySpeed: 2000,    // Adjust for smoother transition time between slides
+    cssEase: 'ease-in-out', // Apply a smooth easing effect
+  };
+  
+
   return (
-  <>
- <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={3}>
-        <Grid size="grow" >
-         <img src="unisex_18260c33-e1e2-4e0b-9d90-ab82b8021578.jpg" alt=""  className='img1'/>
-        </Grid>
-        <Grid item xs={6}>
-            {/* Display images in a single row */}
-            <Grid container spacing={2} direction="row">
-              {imgCarosuel.map((imgSrc, index) => (
-                <Grid item xs={6} key={index}>
-                  <img src={imgSrc} alt={`Carousel ${index}`} className="img-carousel" />
-                </Grid>
-              ))}
-            </Grid>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+      <h1>Trending Collection</h1>
+        <Grid container spacing={3} direction="row" justifyContent="space-between" alignItems="center" >
+      
+          <Grid item xs={3}>
+            <img
+              src="unisex_18260c33-e1e2-4e0b-9d90-ab82b8021578.jpg"
+              alt="Image 1"
+              className="img1"
+            />
           </Grid>
-
-        <Grid size="grow">
-        <img src="hoodie_12d33cd0-c765-46de-89e1-52394b2956d4.jpg" alt="" className='img1'/>
+          <Grid item xs={6}>
+        
+          <Slider {...settings} className='carousel1'>
+          {imgCarosuel.map((imgSrc, index) => ( 
+                  <img src={imgSrc} alt={`Carousel ${index}`} className="img-carousel" />
+              ))}
+          </Slider>
+             
+           
+          </Grid>
+          <Grid item xs={3}>
+            <img
+              src="hoodie_12d33cd0-c765-46de-89e1-52394b2956d4.jpg"
+              alt="Image 2"
+              className="img1"
+            />
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
-  </>
-  )
+      </Box>
+    </>
+  );
 }
 
-export default Section
+export default Section;
