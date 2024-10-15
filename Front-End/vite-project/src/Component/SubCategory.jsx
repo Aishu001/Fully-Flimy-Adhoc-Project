785599cbbb38d97d072cf5ec8a40d7c698ad6b11
Aishu <sowandaryakrishnan@gmail.com>
2024-10-15 17:43:31 +0530

    added the sub category

--- Front-End/vite-project/src/Component/SubCategory.jsx
+++ Front-End/vite-project/src/Component/SubCategory.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 
 function SubCategory() {
   return (
    <>
    
+   <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Sub Category Name</TableCell>
+              <TableCell align="right">Image</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            
+              <TableRow
+               
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                
+                </TableCell>
+                <TableCell align="right">
+            
+                </TableCell>
+              </TableRow>
+         
+          </TableBody>
+        </Table>
+      </TableContainer>
    </>
   )
 }
